de701ffcab90df78ebdb7cce0e9895816c630d2a
Tao Feng <fengtao04@gmail.com>
2016-06-13 23:56:56 -0700

    update

--- 9_palindrome_number.py
+++ 9_palindrome_number.py
@@ -7,3 +7,17 @@ class Solution:
         while a:
             b,a = b*10+a%10, a / 10
         return b == x
+
+
+def isPalindrome(x):
+    if x < 0:
+        return False
+    elif x == 0:
+        return True
+    else:
+        a, b = x, 0
+        while a:
+            b, a = b*10 + a%10, a / 10
+        return b == x
+
+print(isPalindrome(12321))
