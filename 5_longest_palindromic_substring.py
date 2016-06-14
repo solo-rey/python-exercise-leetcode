36124ed98b81fa1c206c7b3c348259a9d120914a
Tao Feng <tofeng@linkedin.com>
2016-06-13 22:48:54 -0700

    comit

--- 5_longest_palindromic_substring.py
+++ 5_longest_palindromic_substring.py
@@ -25,3 +25,24 @@ class Solution:
 
 s = Solution()
 print s.longestPalindrome("ababdc")
+
+
+def longestPalindrome(s):
+    if not s or len(s) <= 1:
+        return s
+    longest = s[0:1]
+    for i in range(0, len(s)):
+        tmp_str = helper(s, i, i)
+        if len(tmp_str) > len(longest):
+            longest = tmp_str
+        tmp_str = helper(s, i, i+1)
+        if len(tmp_str) > len(longest):
+            longest = tmp_str
+    return longest
+
+
+def helper(s, start, end):
+    if start >= 0 and end <= len(s)-1 and s[start] == s[end]:
+        start -= 1
+        end += 1
+    return s[start+1 : end]
