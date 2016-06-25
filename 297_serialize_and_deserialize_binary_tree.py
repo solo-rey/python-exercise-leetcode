6483d939d8e0112b8421e35b42633fec999a57ac
Tao Feng <fengtao04@gmail.com>
2016-06-25 10:56:06 -0700

    297

--- 297_serialize_and_deserialize_binary_tree.py
+++ 297_serialize_and_deserialize_binary_tree.py
@@ -13,3 +13,84 @@ For example, you may serialize the following tree
 as "[1,2,3,null,null,4,5]", just the same as how LeetCode OJ serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
 Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless.
 """
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        st = ''
+
+        def _serialize(root, st):
+            tmp_st = ''
+            if root:
+                tmp_st = (str(root.val) + ' ')
+                tmp_st += _serialize(root.left, st)
+                tmp_st += _serialize(root.right, st)
+
+            else:
+                tmp_st = '# '
+            st += tmp_st
+            return st
+        return _serialize(root, st)
+
+
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        data_list = data.split()
+        def _deserialize(data_list):
+
+            tmp_node = data_list.pop(0)
+            if tmp_node == '#':
+                return None
+            root = TreeNode(int(tmp_node))
+            root.left = _deserialize(data_list)
+            root.right = _deserialize(data_list)
+            return root
+        return _deserialize(data_list)
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
+
+def preorder(root):
+    if root is None:
+        print ' '
+        return
+    print str(root.val) + ' '
+    preorder(root.left)
+    preorder(root.right)
+    return
+
+node1 = TreeNode(1)
+node2 = TreeNode(2)
+node3 = TreeNode(3)
+node4 = TreeNode(4)
+node5 = TreeNode(5)
+node1.left = node2
+node1.right = node3
+node3.left = node4
+node3.right = node5
+codec = Codec()
+"""
+1 2 # # 3 4 # # 5 # #
+"""
+# print codec.serialize(node1)
+root = codec.deserialize(codec.serialize(node1))
+preorder(root)
